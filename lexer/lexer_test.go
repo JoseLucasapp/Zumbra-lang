e26abcbe2e9bb217cf294c2f9e26ebec466e2c57
José Lucas <josluc2016@gmail.com>
2025-04-09 15:10:17 -0300

    feat: lexer

---
+++ lexer/lexer_test.go
@@ -0,0 +1,100 @@
+package lexer
+
+import (
+	"testing"
+
+	"../token"
+)
+
+func TestNextToken(t *testing.T) {
+	input := `var five << 5;
+	var ten << 10.5;
+	
+	var add << fct(x, y) {
+		x + y;
+	};
+	
+	var result << add(five, ten);
+
+	if (5 < 10) {
+		return t;
+	} else {
+		return f;
+	}
+	`
+
+	tests := []struct {
+		expectedType    token.TokenType
+		expectedLiteral string
+	}{
+		{token.LET, "var"},
+		{token.IDENT, "five"},
+		{token.ASSIGN, "<<"},
+		{token.INT, "5"},
+		{token.SEMICOLON, ";"},
+		{token.LET, "var"},
+		{token.IDENT, "ten"},
+		{token.ASSIGN, "<<"},
+		{token.FLOAT, "10.5"},
+		{token.SEMICOLON, ";"},
+		{token.LET, "var"},
+		{token.IDENT, "add"},
+		{token.ASSIGN, "<<"},
+		{token.FUNCTION, "fct"},
+		{token.LPAREN, "("},
+		{token.IDENT, "x"},
+		{token.COMMA, ","},
+		{token.IDENT, "y"},
+		{token.RPAREN, ")"},
+		{token.LBRACE, "{"},
+		{token.IDENT, "x"},
+		{token.PLUS, "+"},
+		{token.IDENT, "y"},
+		{token.SEMICOLON, ";"},
+		{token.RBRACE, "}"},
+		{token.SEMICOLON, ";"},
+		{token.LET, "var"},
+		{token.IDENT, "result"},
+		{token.ASSIGN, "<<"},
+		{token.IDENT, "add"},
+		{token.LPAREN, "("},
+		{token.IDENT, "five"},
+		{token.COMMA, ","},
+		{token.IDENT, "ten"},
+		{token.RPAREN, ")"},
+		{token.SEMICOLON, ";"},
+		{token.IF, "if"},
+		{token.LPAREN, "("},
+		{token.INT, "5"},
+		{token.LT, "<"},
+		{token.INT, "10"},
+		{token.RPAREN, ")"},
+		{token.LBRACE, "{"},
+		{token.RETURN, "return"},
+		{token.TRUE, "t"},
+		{token.SEMICOLON, ";"},
+		{token.RBRACE, "}"},
+		{token.ELSE, "else"},
+		{token.LBRACE, "{"},
+		{token.RETURN, "return"},
+		{token.FALSE, "f"},
+		{token.SEMICOLON, ";"},
+		{token.RBRACE, "}"},
+		{token.EOF, ""},
+
+	}
+
+	l := New(input)
+
+	for i, tt := range tests {
+		tok := l.NextToken()
+
+		if tok.Type != tt.expectedType {
+			t.Fatalf("tests[%d] - tokentype wrong. expected=%q, got=%q", i, tt.expectedType, tok.Type)
+		}
+
+		if tok.Literal != tt.expectedLiteral {
+			t.Fatalf("tests[%d] - literal wrong. expected=%q, got=%q", i, tt.expectedLiteral, tok.Literal)
+		}
+	}
+}
