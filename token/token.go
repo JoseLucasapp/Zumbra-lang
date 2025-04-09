2a3eeb62ae6d32a6a8932d910d40cc9d829412e4
José Lucas <josluc2016@gmail.com>
2025-04-09 15:10:04 -0300

    feat: tokens

---
+++ token/token.go
@@ -0,0 +1,70 @@
+package token
+
+type TokenType string
+
+const (
+	ILLEGAL = "ILLEGAL"
+	EOF = "EOF"
+
+	// Identifiers
+	IDENT = "IDENT"
+	INT = "INT"
+	FLOAT = "FLOAT"
+
+
+	// Operators
+	ASSIGN = "<<"
+	
+	EQUAL= "=="
+	NOT_EQUAL = "!="
+	PLUS = "+"
+	MINUS = "-"
+	BANG = "!"
+	ASTERISK = "*"
+	SLASH = "/"
+	MODULE = "%"
+	LT = "<"
+	GT = ">"
+	POWER = "**"
+	COMMENT = "c/"
+	
+
+	// Delimiters
+	COMMA = ","
+	SEMICOLON = ";"
+	LPAREN = "("
+	RPAREN = ")"
+	LBRACE = "{"
+	RBRACE = "}"
+
+	// Keywords
+	FUNCTION = "FUNCTION"
+	LET = "LET"
+	TRUE = "TRUE"
+	FALSE = "FALSE"
+	IF = "IF"
+	ELSE = "ELSE"
+	RETURN = "RETURN"
+)
+
+type Token struct {
+	Type    TokenType
+	Literal string
+}
+
+var keywords = map[string]TokenType{
+	"fct":  FUNCTION,
+	"var": LET,
+	"t": TRUE,
+	"f": FALSE,
+	"if": IF,
+	"else": ELSE,
+	"return": RETURN,
+}
+
+func LookupIdent(ident string) TokenType {
+	if tok, ok := keywords[ident]; ok {
+		return tok
+	}
+	return IDENT
+}
