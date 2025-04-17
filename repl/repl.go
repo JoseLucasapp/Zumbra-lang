2e8c31ccf7e4768db46fbc7bddc48192ecb7538a
José Lucas <josluc2016@gmail.com>
2025-04-17 14:50:37 -0300

    fix: start vm

--- repl/repl.go
+++ repl/repl.go
@@ -5,17 +5,16 @@ import (
 	"fmt"
 	"io"
 
-	"vaja/evaluator"
+	"vaja/compiler"
 	"vaja/lexer"
-	"vaja/object"
 	"vaja/parser"
+	"vaja/vm"
 )
 
 const PROMPT = ">> "
 
 func Start(in io.Reader, out io.Writer) {
 	scanner := bufio.NewScanner(in)
-	env := object.NewEnvironment()
 
 	for {
 		fmt.Printf(PROMPT)
@@ -34,11 +33,23 @@ func Start(in io.Reader, out io.Writer) {
 			continue
 		}
 
-		evaluated := evaluator.Eval(program, env)
-		if evaluated != nil {
-			io.WriteString(out, evaluated.Inspect())
-			io.WriteString(out, "\n")
+		comp := compiler.New()
+		err := comp.Compile(program)
+		if err != nil {
+			fmt.Fprintf(out, "compiler error: %s\n", err)
+			continue
 		}
+
+		machine := vm.New(comp.Bytecode())
+		err = machine.Run()
+		if err != nil {
+			fmt.Fprintf(out, "vm error: %s\n", err)
+			continue
+		}
+
+		stackTop := machine.StackTop()
+		io.WriteString(out, stackTop.Inspect())
+		io.WriteString(out, "\n")
 	}
 }
 
