b0e6646a0991a592699e0963cd2555c0ace193f7
José Lucas <josluc2016@gmail.com>
2025-05-19 12:06:52 -0300

    feat: new show builtin

--- object/builtins/show_builtin.go
+++ object/builtins/show_builtin.go
@@ -2,16 +2,41 @@ package builtins
 
 import (
 	"fmt"
+	"strings"
 	"zumbra/object"
 )
 
 func ShowBuiltin() *object.Builtin {
 	return &object.Builtin{
 		Fn: func(args ...object.Object) object.Object {
-			for _, arg := range args {
-				fmt.Println(arg.Inspect())
+
+			if len(args) == 0 {
+				fmt.Println()
+				return nil
 			}
+
+			if len(args) == 1 {
+				fmt.Println(args[0].Inspect())
+				return nil
+			}
+
+			formatObj, ok := args[0].(*object.String)
+
+			if !ok {
+				return NewError("First argument to `show` must be STRING, got %s", args[0].Type())
+			}
+			format := formatObj.Value
+			values := []interface{}{}
+
+			for _, arg := range args[1:] {
+				values = append(values, arg.Inspect())
+			}
+
+			formatConverted := strings.ReplaceAll(format, "{}", "%v")
+
+			fmt.Printf(formatConverted+"\n", values...)
 			return nil
+
 		},
 	}
 }
