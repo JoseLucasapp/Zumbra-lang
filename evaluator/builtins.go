c41b56b1149674a81d6605b63bde845a0b86194c
José Lucas <alibhassan6969@gmail.com>
2025-05-29 11:34:34 -0300

    feat: organize

--- evaluator/builtins.go
+++ evaluator/builtins.go
@@ -8,14 +8,52 @@ import (
 var builtinsList = make(map[string]*object.Builtin)
 
 func init() {
-	names := []string{
-		"addToArrayStart", "addToArrayEnd", "addToDict", "allButFirst", "bhaskara", "capitalize", "date", "deleteFromDict", "dictKeys", "dictValues",
-		"first", "get", "getFromDict", "html", "indexOf", "input", "json_parse", "last", "max", "min", "organize", "randomFloat", "randomInteger",
-		"registerRoute", "removeFromArray", "removeWhiteSpaces", "replace", "sendEmail", "sendWhatsapp", "server", "serveFile", "serveStatic",
-		"show", "sizeOf", "sum", "toBool", "toFloat", "toInt", "toLowercase", "toString", "toUppercase",
+	arrays := []string{
+		"addToArrayStart", "addToArrayEnd", "allButFirst", "first", "indexOf", "last", "max", "min", "organize", "removeFromArray", "sizeOf", "sum",
 	}
 
-	for _, name := range names {
+	dicts := []string{
+		"addToDict", "deleteFromDict", "dictKeys", "dictValues", "getFromDict",
+	}
+
+	http := []string{
+		"get", "html", "registerRoute", "server", "serveFile", "serveStatic",
+	}
+
+	parsers := []string{
+		"json_parse", "toBool", "toFloat", "toInt", "toString",
+	}
+
+	stringUtils := []string{
+		"capitalize", "removeWhiteSpaces", "replace", "toLowercase", "toUppercase",
+	}
+
+	numbersUtils := []string{
+		"bhaskara", "randomFloat", "randomInteger",
+	}
+
+	ioUtils := []string{
+		"input", "show",
+	}
+
+	messages := []string{
+		"sendEmail", "sendWhatsapp",
+	}
+
+	extras := []string{
+		"date",
+	}
+
+	allBuiltins := append(arrays, dicts...)
+	allBuiltins = append(allBuiltins, http...)
+	allBuiltins = append(allBuiltins, parsers...)
+	allBuiltins = append(allBuiltins, stringUtils...)
+	allBuiltins = append(allBuiltins, numbersUtils...)
+	allBuiltins = append(allBuiltins, ioUtils...)
+	allBuiltins = append(allBuiltins, messages...)
+	allBuiltins = append(allBuiltins, extras...)
+
+	for _, name := range allBuiltins {
 		if builtin := builtins.GetBuiltinByName(name); builtin != nil {
 			builtinsList[name] = builtin
 		}
