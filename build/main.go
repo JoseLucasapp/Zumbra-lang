263dd8f4bec3ca424de78efd76eb6d9cb8308a26
José Lucas <jlgf.profissional@gmail.com>
2025-06-10 12:15:49 -0300

    feat: build

---
+++ build/main.go
@@ -0,0 +1,10 @@
+package main
+		import "fmt"
+
+		func main() {
+			    fmt.Printf("\"%v + %v = %v\"", 1, 1, 1+1); //output: 1 + 1 = 2)
+    fmt.Printf("\"%v\"", 1); //output: 1)
+    fmt.Println("\"{}\"); //output: {}")
+    fmt.Printf("\"Zumbra\"); //output: Zumbra")
+		}
+	
--- main.go
+++ main.go
@@ -4,8 +4,10 @@ import (
 	"fmt"
 	"io/ioutil"
 	"os"
+	"os/exec"
 	"os/user"
 	"path/filepath"
+	"strings"
 
 	"zumbra/compiler"
 	"zumbra/lexer"
@@ -20,6 +22,14 @@ func main() {
 	user, err := user.Current()
 	if err != nil {
 		panic(err)
+	}
+
+	if len(os.Args) > 2 && os.Args[1] == "build" {
+		err := buildZumbra(os.Args[2])
+		if err != nil {
+			fmt.Printf("Error when trying to build the file: %s\n", err)
+		}
+		return
 	}
 
 	if len(os.Args) > 1 {
@@ -88,4 +98,117 @@ func runFile(filename string) {
 	}
 
 	machine.LastPoppedStackElem()
+}
+
+func buildZumbra(filename string) error {
+	data, err := os.ReadFile(filename)
+	if err != nil {
+		return fmt.Errorf("Error when trying to read the file: %s\n", err)
+	}
+
+	source := string(data)
+	goCode, err := ZumbraTranspiler(source)
+	if err != nil {
+		return fmt.Errorf("erro ao transpilar: %w", err)
+	}
+
+	if _, err := os.Stat("build"); err == nil {
+		err := os.RemoveAll("build")
+		if err != nil {
+			return fmt.Errorf("Error when trying to remove the file: %w", err)
+		}
+	}
+
+	err = os.MkdirAll("build", 0755)
+	if err != nil {
+		return fmt.Errorf("Error when trying to create the file: %w", err)
+	}
+
+	err = os.WriteFile("build/main.go", []byte(goCode), 0644)
+	if err != nil {
+		return fmt.Errorf("Error when trying to write the file: %w", err)
+	}
+
+	cmd := exec.Command("go", "build", "-o", "build/zumbra-app", "build/main.go")
+	cmd.Stdout = os.Stdout
+	cmd.Stderr = os.Stderr
+	return cmd.Run()
+}
+
+func ZumbraTranspiler(zum string) (string, error) {
+	lines := strings.Split(zum, "\n")
+	var goBody []string
+
+	for _, line := range lines {
+		line = strings.TrimSpace(line)
+
+		if strings.HasPrefix(line, "show(") {
+			content := strings.TrimPrefix(line, "show(")
+			content = strings.TrimSuffix(content, ")")
+
+			args := splitArgs(content)
+
+			if len(args) == 0 {
+				goBody = append(goBody, `    fmt.Println()`)
+				continue
+			}
+
+			format := args[0]
+			placeholders := strings.Count(format, "{}")
+			formatGo := strings.ReplaceAll(format, "{}", "%v")
+
+			if placeholders > 0 && len(args)-1 < placeholders {
+				goBody = append(goBody, fmt.Sprintf(`    fmt.Println(%q)`, format))
+				continue
+			}
+
+			line := fmt.Sprintf(`    fmt.Printf(%q`, formatGo)
+			if len(args) > 1 {
+				line += ", " + strings.Join(args[1:], ", ")
+			}
+			line += ")"
+			goBody = append(goBody, line)
+
+		}
+
+		if strings.HasPrefix(line, "var ") {
+			line = strings.ReplaceAll(line, "<<", "=")
+			goBody = append(goBody, "    "+line)
+		}
+
+	}
+
+	return fmt.Sprintf(
+		`package main
+		import "fmt"
+
+		func main() {
+			%s
+		}
+	`, strings.Join(goBody, "\n")), nil
+}
+
+func splitArgs(input string) []string {
+	var args []string
+	var curr strings.Builder
+	inStr := false
+
+	for i := 0; i < len(input); i++ {
+		ch := input[i]
+
+		if ch == '"' {
+			inStr = !inStr
+		}
+
+		if ch == ',' && !inStr {
+			args = append(args, strings.TrimSpace(curr.String()))
+			curr.Reset()
+		} else {
+			curr.WriteByte(ch)
+		}
+	}
+	if curr.Len() > 0 {
+		args = append(args, strings.TrimSpace(curr.String()))
+	}
+	return args
 }
